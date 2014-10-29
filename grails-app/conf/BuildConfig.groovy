111bd842aa6181ee3f42fa561802313319f1b464
Shin <shin@endource.com>
2014-10-29 14:23:54 +0000

    bintray repo details - username/password assumed to be in your ~/.grails/settings.groovy (see BuildConfig.groovy)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,13 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
 
+grails.project.repos.default = "bintray"
+grails.project.repos.bintray.url = "https://api.bintray.com/maven/endource/maven/grails-database-session"
+
+// Set these up in your ~/.grails/settings.groovy with your bintray username and API key
+//    grails.project.repos.bintray.username = "admin"
+//    grails.project.repos.bintray.password = "password"
+
 grails.project.dependency.resolution = {
 
 	inherits 'global'
