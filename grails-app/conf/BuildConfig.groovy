58d69929682e15cbe3aab4508c98a77f7b91ab04
shin <shin@endource.com>
2015-03-27 11:41:45 +0000

    added back other repos to resolve deps

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,14 +9,32 @@ grails.project.repos.bintray.url = "https://api.bintray.com/maven/endource/maven
 //    grails.project.repos.bintray.password = "password"
 
 grails.project.dependency.resolution = {
-
-	inherits 'global'
-	log 'warn'
+	// inherit Grails' default dependencies
+	inherits("global") {
+		// specify dependency exclusions here; for example, uncomment this to disable ehcache:
+		// excludes 'ehcache'
+	}
+	log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	checksums true // Whether to verify checksums on resolve
+	legacyResolve true // whether to do a secondary resolve on plugin installation, not advised but here for backwards compatibility
 
 	repositories {
-		grailsCentral()
-	}
+		inherits true // Whether to inherit repository definitions from plugins
 
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+
+		mavenLocal()
+		mavenCentral()
+
+		// uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+		//mavenRepo "http://snapshots.repository.codehaus.org"
+		//mavenRepo "http://repository.codehaus.org"
+		//mavenRepo "http://download.java.net/maven/2/"
+		//mavenRepo "http://repository.jboss.com/maven2/"
+	}
+	
 	plugins {
 
 		runtime ":hibernate:$grailsVersion", {
